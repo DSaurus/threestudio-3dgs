85828105942e069fd575d23f938a5ea7794d390c
DSaurus <2238454358@qq.com>
2023-12-04 22:36:55 +0800

    update init

--- __init__.py
+++ __init__.py
@@ -1,3 +1,9 @@
+from .background import gaussian_mvdream_background
 from .geometry import gaussian
-from .system import gaussian_splatting, gaussian_mvdream
-from .renderer import diff_gaussian_rasterizer
+from .material import gaussian_material
+from .renderer import (
+    diff_gaussian_rasterizer,
+    diff_gaussian_rasterizer_background,
+    diff_gaussian_rasterizer_shading,
+)
+from .system import gaussian_mvdream, gaussian_splatting
