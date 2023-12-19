8c3d861620f562c55fa652ebcc1f508d1d9f5eba
DSaurus <2238454358@qq.com>
2023-12-20 01:52:18 +0800

    export

--- __init__.py
+++ __init__.py
@@ -1,8 +1,9 @@
 from .background import gaussian_mvdream_background
-from .geometry import gaussian
+from .geometry import exporter, gaussian, gaussian_base
 from .material import gaussian_material
 from .renderer import (
     diff_gaussian_rasterizer,
+    diff_gaussian_rasterizer_advanced,
     diff_gaussian_rasterizer_background,
     diff_gaussian_rasterizer_shading,
 )
