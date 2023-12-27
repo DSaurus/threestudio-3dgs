dea783d99ba5b72c9518078fd45bd94fd7974593
DSaurus <2238454358@qq.com>
2023-12-28 00:15:35 +0800

    update version

--- __init__.py
+++ __init__.py
@@ -1,3 +1,18 @@
+import threestudio
+from packaging.version import Version
+
+if hasattr(threestudio, "__version__") and Version(threestudio.__version__) >= Version(
+    "0.2.1"
+):
+    pass
+else:
+    if hasattr(threestudio, "__version__"):
+        print(f"[INFO] threestudio version: {threestudio.__version__}")
+    raise ValueError(
+        "threestudio version must be >= 0.2.0, please update threestudio by pulling the latest version from github"
+    )
+
+
 from .background import gaussian_mvdream_background
 from .geometry import exporter, gaussian, gaussian_base
 from .material import gaussian_material
